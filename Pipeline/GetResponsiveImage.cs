6581b64458e71b7feb5521fe06ce3e35577ee58c
Derek Temple <xalor90@outlook.com>
2018-07-09 09:42:13 -0700

    Set up project and install instructions

---
+++ Pipeline/GetResponsiveImage.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Pipelines.RenderField;
+using System.Text.RegularExpressions;
+
+namespace Sitecore.Foundation.Extension.ResponsiveImage.Pipeline
+{
+    public class GetResponsiveImage
+    {
+        public void Process(RenderFieldArgs args)
+        {
+            if (args.FieldTypeKey != "image")
+            {
+                return;
+            }
+
+            if (args.Parameters.ContainsKey("responsive"))
+            {
+                string imageTag = args.Result.FirstPart;
+                imageTag = Regex.Replace(imageTag, @"(<img[^>]*?)\s+height\s*=\s*\S+", "$1", RegexOptions.IgnoreCase);
+                imageTag = Regex.Replace(imageTag, @"(<img[^>]*?)\s+width\s*=\s*\S+", "$1", RegexOptions.IgnoreCase);
+                imageTag = Regex.Replace(imageTag, @"(<img[^>]*?)\s+responsive\s*=\s*\S+", "$1", RegexOptions.IgnoreCase);
+                args.Result.FirstPart = imageTag;
+            }
+        }
+    }
+}
